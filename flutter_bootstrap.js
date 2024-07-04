fe0570733ddce8b2e25d907844bd3dba3835ed72
Bhargav-eCEOs <147034576+eCEOsATS@users.noreply.github.com>
2024-07-04 10:19:49 +0800

    Merged Auction UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3890693525"
+    serviceWorkerVersion: "2632246908"
   }
 });
