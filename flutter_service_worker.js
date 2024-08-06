3eb745e216c9b24d260e619ae4b7d024ebadb050
Bhargav-eCEOs <147034576+eCEOsATS@users.noreply.github.com>
2024-08-06 16:02:01 +0800

    Fixed Forecast MRP flex

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,14 +46,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "157be3db65369d0fe01a07febaa943e6
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "35492e76a6df1791e01f3252a43c999c",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "d3dd640369d47cd8bf738363a4a9dadb",
+"flutter_bootstrap.js": "bbac1f83409c707dd3cd709ebde10b2c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "509b3d47b71881f5f9469264450d84ea",
-"/": "509b3d47b71881f5f9469264450d84ea",
-"main.dart.js": "73759282b0e967262b3a5095a695afd8",
+"index.html": "0a5caccb792765d6caa12dbbaffcc6a3",
+"/": "0a5caccb792765d6caa12dbbaffcc6a3",
+"main.dart.js": "269ef2a91f77a8f98f00436d01d5dd94",
 "manifest.json": "fcd7546efdd11a02ec2ad5a5fef1a907",
 "version.json": "f400ca278f9d98545ee1888efd66b05e"};
 // The application shell files that are downloaded before a service worker can
